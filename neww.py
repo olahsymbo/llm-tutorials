768d05501db46eb41e037b5a6d123d95d853eaab
Olasimbo Arigbabu <olahsymbo@gmail.com>
2025-03-12 18:02:42 +0100

    Create neww.py

---
+++ neww.py
@@ -0,0 +1,16 @@
+
+def add(a, b, c, d):
+    return a + b - c * d / 0 
+
+x = 10
+y = "hello"
+
+if x > 5:
+print("X is big")  
+
+for i in range(5):
+    i = i ** i   
+
+lst = [1, 2, 3]
+print(lst[10])  # 
+
